7544c5b310faab8854c6ec862d29c1ae262c6d77
Harsh <pariharharsh337@gmail.com>
2023-11-11 16:19:51 +0530

    Connected to data base

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const dbConnect = require("./config/dbconnect");
 const app = express();
-const dotenv = require("dotenv").config();
-const PORT = process.env.PORT || 4000;
+const dotenv = require('dotenv')
+dotenv.config();
+
+// const PORT = process.env.PORT || 3000;
+const PORT = 5000;
+dbConnect();
+
+
+app.use("/",(req,res)=>{
+    res.send("Hello from the server side");
+})
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on the port ${PORT}`)
