5c92a09a21e733c24cc6d97de410e06dcdd4d067
Harsh <pariharharsh337@gmail.com>
2023-12-04 14:39:54 +0530

    Done with the routes of register and login

--- controller/userController.js
+++ controller/userController.js
@@ -11,13 +11,31 @@ const createUser = asyncHandler(async (req, res) => {
     if(!findUser){
         // Create a new user
         const newUser = await User.create(req.body);
-        res.json(newUser);
-        console.log("creating  user")
+        res.send(newUser);
     }
     else{
         //User already exist
+    //   res.json({
+    //     msg : "User already exist",
+    //     success : false,
+    //   })
       throw new Error("User already exist........")
     }
 })
 
-module.exports = { createUser }
+const loginUserCtrl = asyncHandler(async (req,res)=>{
+    const {email , password} = req.body;
+    // Check if user exist or not
+
+    const findUser = await User.findOne({email : email})
+    
+    if(findUser && await findUser.isPasswordMatched(password)){
+        res.json(findUser)
+    }
+    else{
+        throw new Error("Invalid credentials")
+    }
+
+})
+
+module.exports = { createUser , loginUserCtrl }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+// const dotenv = require('dotenv')
+// dotenv.config();
+
 const express = require("express");
 const dbConnect = require("./config/dbconnect");
 const app = express();
 const bodyPaser = require("body-parser")
-// const dotenv = require('dotenv')
-// dotenv.config();
 const authRouter = require("./routes/authRoutes");
 const { notFound, errorHandler } = require("./middleware/errorHandling");
 
@@ -11,18 +12,18 @@ const { notFound, errorHandler } = require("./middleware/errorHandling");
 const PORT = 5000;
 
 dbConnect();
-// app.use(express.json())
-app.use(bodyPaser.json());
-app.use(bodyPaser.urlencoded({extended : false}))
+app.use(express.json())
+// app.use(bodyPaser.json());
+// app.use(bodyPaser.urlencoded({extended : false}))
 
 app.use("/api/user", authRouter);
-
-app.use(notFound)
-app.use(errorHandler);
 
 app.use("/",(req,res)=>{
     res.send("Hello from the server side");
 })
+
+app.use(notFound)
+app.use(errorHandler);
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on the port ${PORT}`)
