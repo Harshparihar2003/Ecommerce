1c60a4e69952910f9ea03e0b33b07d2f244fa568
Harsh <pariharharsh337@gmail.com>
2023-12-04 17:33:55 +0530

    Done with all the routes

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,9 @@
+const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 
+
+// Create a user
 const createUser = asyncHandler(async (req, res) => {
     const email = req.body.email;
 
@@ -23,14 +26,22 @@ const createUser = asyncHandler(async (req, res) => {
     }
 })
 
+// Login a user
 const loginUserCtrl = asyncHandler(async (req,res)=>{
     const {email , password} = req.body;
     // Check if user exist or not
 
     const findUser = await User.findOne({email : email})
-    
+
     if(findUser && await findUser.isPasswordMatched(password)){
-        res.json(findUser)
+        res.json({
+            _id : findUser?.id,
+            firstname : findUser?.firstName,
+            lastname : findUser?.lastName,
+            email : findUser?.email,
+            mobile : findUser?.mobile,
+            token : generateToken(findUser?._id)
+        })
     }
     else{
         throw new Error("Invalid credentials")
@@ -38,4 +49,75 @@ const loginUserCtrl = asyncHandler(async (req,res)=>{
 
 })
 
-module.exports = { createUser , loginUserCtrl }
+// Updata a user
+
+const updateUser = asyncHandler(async (req,res)=>{
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+        });
+        if(updateUser){
+            res.json({
+                msg : "User updated successfully",
+                success : true,
+            })
+        }
+        else{
+            throw new Error("User not found")
+        }
+    } catch (error) {
+        throw new Error(error)
+    }
+
+})
+
+
+// Get all the user
+const getAllUser = asyncHandler(async (req,res)=>{
+    try {
+        const getUsers = await User.find();
+        res.json(getUsers)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+// Get a user
+
+const getAUser = asyncHandler(async (req,res)=>{
+    try {
+        const getUser = await User.findById(req.params.id);
+        if(getUser){
+            res.json(getUser)
+        }
+        else{
+            throw new Error("User not found")
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+// Delete a user
+const deleteAUser = asyncHandler(async (req,res)=>{
+    try {
+        const deleteUser = await User.findByIdAndDelete(req.params.id);
+        if(deleteUser){
+            res.json({
+                msg : "User deleted successfully",
+                success : true
+            })
+        }
+        else{
+            throw new Error("User not found")
+        }
+    } catch (error) {
+        throw new Error(error);
+    }
+})
+
+
+
+
+module.exports = { createUser , loginUserCtrl , getAllUser , getAUser , deleteAUser , updateUser}
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const {createUser , loginUserCtrl} = require("../controller/userController")
+const {createUser , loginUserCtrl, getAllUser, getAUser, deleteAUser, updateUser} = require("../controller/userController")
 
 
 router.post("/register", createUser);
 router.post("/login", loginUserCtrl);
+router.get("/getuser", getAllUser);
+router.get("/:id", getAUser)
+router.delete("/:id", deleteAUser)
+router.put("/:id", updateUser)
 module.exports = router;
