919fb3514f61768a59c9a25e09c31d579cc74dfa
Harsh <pariharharsh337@gmail.com>
2023-12-19 17:10:12 +0530

    Added frontend part

--- E-commerce/BACKEND/config/jwtToken.js
+++
@@ -1,8 +0,0 @@
-const jwt = require("jsonwebtoken")
-const secret_key = "secretkey";
-
-const generateToken = (id) =>{
-    return jwt.sign({id}, secret_key, {expiresIn : "1d"})
-};
-
-module.exports = { generateToken }
