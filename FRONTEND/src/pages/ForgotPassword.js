7aeb97d34790c2e1b075e3a87a9a64d02a4f6226
Harsh <pariharharsh337@gmail.com>
2023-12-23 01:10:34 +0530

    Completed the Wishlist page

---
+++ FRONTEND/src/pages/ForgotPassword.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import BreadCrumb from '../components/BreadCrumb'
+import Meta from '../components/Meta'
+
+const ForgotPassword = () => {
+  return (
+    <>
+    <Meta title="Login" />
+    <BreadCrumb title="Login" />
+    </>
+  )
+}
+
+export default ForgotPassword
