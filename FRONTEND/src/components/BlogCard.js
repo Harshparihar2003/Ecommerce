c0c91a0143cea77f9ed8976f05c1fc351d555de7
Harsh <pariharharsh337@gmail.com>
2023-12-22 14:50:14 +0530

    initial commit

---
+++ FRONTEND/src/components/BlogCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NavLink, Link } from "react-router-dom"
+
+const BlogCard = () => {
+  return (
+    <>
+      <div className="col-3">
+        <div className="blog-card">
+          <div className="card-image">
+            <img src="images/blog-1.jpg" className='img-fluid' alt="blog" />
+          </div>
+          <div className="blog-content">
+            <p className="date">
+              1 Dec, 2023
+            </p>
+            <h5 className="title">
+              A beautiful sunday morning renaissance
+            </h5>
+            <p className="desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam dolorem fugit veniam voluptates facere error ad enim adipisci nostrum nesciunt maxime impedit soluta cupiditate eaque, exercitationem nemo ipsam omnis amet pariatur ipsum cum atque libero asperiores? Incidunt eligendi dolorum accusantium?</p>
+            <Link className="button" to="/">Read More</Link>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default BlogCard
