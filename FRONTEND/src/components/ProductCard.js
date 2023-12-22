00bbd291df433cb10720d4770100fb8dca47b26c
Harsh <pariharharsh337@gmail.com>
2023-12-22 19:33:26 +0530

    Done with Our store page

--- FRONTEND/src/components/ProductCard.js
+++ FRONTEND/src/components/ProductCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactStars from "react-rating-stars-component";
-import { NavLink, Link } from "react-router-dom"
+import { NavLink, Link, useLocation } from "react-router-dom"
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+    let location = useLocation();
+    const {grid} = props;
   return (
     <>
-        <div className="col-3">
+        <div className={`${location.pathname=="/store" ? `gr-${grid}`: "col-3"}`}>
             <div className="product-card position-relative">
                 <div className="wishlist-icon position-absolute">
                     <Link><img src="images/wish.svg" alt="wishlist" /></Link>
@@ -20,6 +22,36 @@ const ProductCard = () => {
                         Kids headphones bulk 10 pack multi colored for students.
                     </h5>
                     <ReactStars count={5} size={24} value="3" activeColor="#ffd700" edit={false}/>
+                    <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos deserunt laboriosam consequuntur? Nobis iste placeat veniam quia illo quidem maxime!</p>
+                    <p className="price">$100.00</p>
+                </div>
+                <div className="action-bar position-absolute">
+                    <div className="d-flex flex-column gap-15">
+                        <Link><img src="images/prodcompare.svg" alt="Product compare" /></Link>
+                        <Link><img src="images/view.svg" alt="view" /></Link>
+                        <Link><img src="images/add-cart.svg" alt="add-cart" /></Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={`${location.pathname=="/store" ? `gr-${grid}`: "col-3"}`}>
+            <div className="product-card position-relative">
+                <div className="wishlist-icon position-absolute">
+                    <Link><img src="images/wish.svg" alt="wishlist" /></Link>
+                </div>
+                <div className="product-image">
+                    <img src="images/watch.jpg" className='img-fluid' alt="product image" />
+                    <img src="images/watch.jpg" className='img-fluid' alt="product image" />
+                </div>
+                <div className="product-details">
+                    <h6 className="brand">Havels</h6>
+                    <h5 className="product-title">
+                        Kids headphones bulk 10 pack multi colored for students.
+                    </h5>
+                    <ReactStars count={5} size={24} value="3" activeColor="#ffd700" edit={false}/>
+                    <p className={`description ${grid === 12 ? "d-block" : "d-none"}`}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptates consequuntur quisquam quae mollitia? Soluta accusantium corrupti eius rem nihil. Nisi.</p>
                     <p className="price">$100.00</p>
                 </div>
                 <div className="action-bar position-absolute">
