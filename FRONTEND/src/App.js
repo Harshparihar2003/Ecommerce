423e575585f8f62b753a2706afa340b016c9aea6
Harsh <pariharharsh337@gmail.com>
2023-12-23 02:20:43 +0530

    Done with all authentication page

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -9,6 +9,10 @@ import OurStore from './pages/OurStore';
 import Blogs from './pages/Blogs';
 import CompareProduct from './pages/CompareProduct';
 import Wishlist from './pages/Wishlist';
+import Login from './pages/Login';
+import ForgotPassword from './pages/ForgotPassword';
+import SignUp from './pages/SignUp';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
@@ -23,6 +27,10 @@ function App() {
           <Route path="blogs" element={<Blogs/>}/>
           <Route path="compare-product" element={<CompareProduct/>}/>
           <Route path="wishlist" element={<Wishlist/>}/>
+          <Route path="login" element={<Login/>}/>
+          <Route path="forgot-password" element={<ForgotPassword/>}/>
+          <Route path="signup" element={<SignUp/>}/>
+          <Route path="reset-password" element={<ResetPassword/>}/>
         </Route>
         </Routes>
       </BrowserRouter>
